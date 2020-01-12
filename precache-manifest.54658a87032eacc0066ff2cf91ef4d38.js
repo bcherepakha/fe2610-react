5fadcc2d33e3f2aa06a75c3b7260fd28cfc40e69
Boris Cherepakha <bcherepakha@gmail.com>
2020-01-12 06:02:00 +0200

    Updates

---
+++ precache-manifest.54658a87032eacc0066ff2cf91ef4d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cd34c07d5a71a6a2c58e822c87e840",
+    "url": "/fe2610-react/index.html"
+  },
+  {
+    "revision": "befa419249fc4bc90ffb",
+    "url": "/fe2610-react/static/css/main.73703d02.chunk.css"
+  },
+  {
+    "revision": "e2c9c5ec9e7defd7e669",
+    "url": "/fe2610-react/static/js/2.88fbf253.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fe2610-react/static/js/2.88fbf253.chunk.js.LICENSE"
+  },
+  {
+    "revision": "befa419249fc4bc90ffb",
+    "url": "/fe2610-react/static/js/main.058c7709.chunk.js"
+  },
+  {
+    "revision": "ed8e3c84ac6c0e8a9289",
+    "url": "/fe2610-react/static/js/runtime-main.8bf0f03b.js"
+  }
+]);
